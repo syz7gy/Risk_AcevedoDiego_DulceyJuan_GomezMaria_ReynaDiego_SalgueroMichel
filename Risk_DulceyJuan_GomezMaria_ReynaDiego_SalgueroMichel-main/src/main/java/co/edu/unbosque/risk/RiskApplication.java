23adca579a170db283135788eb31461f95725640
MpGomezg <141039887+MpGomezg@users.noreply.github.com>
2023-11-17 08:42:40 -0500

    set the user name created by juanpeace

---
+++ Risk_DulceyJuan_GomezMaria_ReynaDiego_SalgueroMichel-main/src/main/java/co/edu/unbosque/risk/RiskApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.risk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiskApplication.class, args);
+	}
+
+}
---
+++ Risk_DulceyJuan_GomezMaria_ReynaDiego_SalgueroMichel-main/src/test/java/co/edu/unbosque/risk/RiskApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.risk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
